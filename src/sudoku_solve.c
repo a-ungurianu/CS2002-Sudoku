b09ae664b8f652cb8f2f4156534b1ee0adf88ae8
Alex Ungurianu <iau2@st-andrews.ac.uk>
2017-02-24 16:39:05 +0000

    Initial scaffolding for sudoku_solver

--- src/sudoku_solve.c
+++ src/sudoku_solve.c
@@ -1,10 +1,31 @@
 #include "sudoku.h"
 #include "sudoku_io.h"
+#include <stdlib.h>
+
+sudoku *solve_sudoku(sudoku *givenSudoku) {
+    sudoku *solvedSudoku = copy_sudoku(givenSudoku);
+    return solvedSudoku;
+}
 
 int main() {
     sudoku * givenSudoku = read_sudoku(stdin);
 
+    switch (check_sudoku(givenSudoku)) {
+        case CR_INVALID:
+            printf("%s\n", "UNSOLVABLE");
+            break;
+        case CR_COMPLETE:
+            write_sudoku(stdout, givenSudoku);
+            break;
+        case CR_INCOMPLETE:
+            ; // Makes the variable initalization below work
+            sudoku *solvedSudoku = solve_sudoku(givenSudoku);
+            write_sudoku(stdout, givenSudoku);
+            free_sudoku(solvedSudoku);
+            break;
+    }
 
+    free_sudoku(givenSudoku);
 
     return 0;
 }
