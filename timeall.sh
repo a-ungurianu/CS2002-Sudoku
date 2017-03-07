fbdedc5d409bd8813b68b4017236d5bd17c48650
Alex Ungurianu <iau2@st-andrews.ac.uk>
2017-03-07 18:14:05 +0000

    Add comparison between basic and advanced in the script

--- timeall.sh
+++ timeall.sh
@@ -2,6 +2,8 @@
 
 for f in $(find $1 -iname '*.in' | sort)
 do
-    echo -n "$(basename $f): "
+    echo -n "Basic - $(basename $f): "
+    /usr/bin/time -f "%e s" ./sudoku_solver < $f > /dev/null
+    echo -n "Advanced - $(basename $f): "
     /usr/bin/time -f "%e s" ./sudoku_advanced < $f > /dev/null
 done
